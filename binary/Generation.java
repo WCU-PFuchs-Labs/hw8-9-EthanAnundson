ab635bb19b4f9003e9237f5a018b2f8ba68d4eb4
EthanAnundson <112589624+EthanAnundson@users.noreply.github.com>
2025-12-05 13:23:08 -0500

    Update Generation.java

--- binary/Generation.java
+++ binary/Generation.java
@@ -1,9 +1,17 @@
 package binary;
+import java.util.Random;
 
 public class Generation{
+  
   public Generation(int size, int maxDepth, String fileName){
     DataSet set = new DataSet(fileName);
-    
+    Random rand = new Random();
+    GPTree[] population = new GPTree[size];
+    Binop[] ops = {new Plus(), new Minus(), new Mult(), new Divide()};
+    NodeFactory box = new NodeFactory(ops, set.getNumIndepVars());
+    for (int i = 0; i<= size; i++){
+      population[i] = new GPTree(box, maxDepth, rand);  
+    }
   }
 
 }
