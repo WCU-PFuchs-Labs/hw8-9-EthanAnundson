0e40f5f95d0610179b805ffa8e294ece890a27c1
EthanAnundson <112589624+EthanAnundson@users.noreply.github.com>
2025-12-05 14:41:13 -0500

    Update Generation.java

--- binary/Generation.java
+++ binary/Generation.java
@@ -2,16 +2,52 @@ package binary;
 import java.util.Random;
 
 public class Generation{
+  private GPTree[] population;
   
   public Generation(int size, int maxDepth, String fileName){
     DataSet set = new DataSet(fileName);
     Random rand = new Random();
-    GPTree[] population = new GPTree[size];
+    population = new GPTree[size];
     Binop[] ops = {new Plus(), new Minus(), new Mult(), new Divide()};
     NodeFactory box = new NodeFactory(ops, set.getNumIndepVars());
     for (int i = 0; i<= size; i++){
       population[i] = new GPTree(box, maxDepth, rand);  
     }
   }
+  public void evalAll(){
+    for (int p = 0; p<= population.length; p++){
+      population[p].eval(set);
+    }
+    Arrays.sort(population);
+  }
+  public ArrayList<GPTree> getTopTen(){
+    ArrayList<GPTree> list = new ArrayList<>();
+    for (int r = 0; r <= 10; r++){
+      list.add(population[size+r-11];
+    }
+    return list;
+  }
+  public void printBestFitness(){
+    System.out.println(""+population[size-1].getFitness());
+  }
+  public void printBestTree(){
+    System.out.println(population[size-1].toString());
+  }
+  public void evolve(){
+    for (int i = 0; i < population.length; i += 2) {
+      int a = rand.nextInt(population.length/2 -1) + population.length/2;
+      int b = rand.nextInt(population.length/2 -1) + population.length/2;
+      GPTree first = population[a].clone();
+      GPTree second = population[b].clone();
+
+            first.crossover(second, rand);
+
+            newPop[i] = first;
+            if (i + 1 < population.length) {
+                newPop[i + 1] = second;
+            }
+        }
+        this.population = newPop;
+    }
 
 }
