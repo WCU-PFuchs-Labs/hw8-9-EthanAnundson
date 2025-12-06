fad761970dcbbf4a87b4b0eaad2ddf1142081cd8
EthanAnundson <112589624+EthanAnundson@users.noreply.github.com>
2025-12-05 23:59:15 -0500

    Update TestGP.java

--- TestGP.java
+++ TestGP.java
@@ -4,10 +4,10 @@ import binary.*; // GPTree, NodeFactory, DataSet, Binop, etc.
 public class TestGP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Whats your file name: ");
+        //System.out.println("Whats your file name: ");
         String filename = sc.next();
         Generation gen = new Generation(500, 4, filename);
-        for(int i = 0; i<50; i++){
+        for(int i = 1; i<50; i++){
             gen.evolve();
             System.out.println("Generation "+i+": ");
             gen.printBestTree();
