62d97d0dee772bd58de6020ba241219e6ab8f29e
EthanAnundson <112589624+EthanAnundson@users.noreply.github.com>
2025-12-05 19:21:22 -0500

    Update TestGeneration.java

--- TestGeneration.java
+++ TestGeneration.java
@@ -29,7 +29,7 @@ public class TestGeneration {
         ArrayList<GPTree> topTen = gen.getTopTen();
 
         // Print formatted fitness values
-        System.out.println("Top Ten Fitness Values:");
+        System.out.print("Top Ten Fitness Values:");
         printTopTenFitness(topTen);
     }
 
